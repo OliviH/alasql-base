03e9aaf72c2742fd21c78b6aab2d36ca388ffc65
olhei <olivier.heimerdinger@smile.fr>
2022-03-13 15:00:22 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import express from "express"
+
+const app = express()
+
+const PORT = process.env.PORT || 5600
+
+app.set('view engine', 'ejs')
+
+app.get('/', function(req, res) {
+    res.render('index', {title:'Page de base',date: new Date().toLocaleString()})
+})
+
+app.listen(PORT,()=>{
+    console.log(`server listen at http://localhost:${PORT}`)
+})
